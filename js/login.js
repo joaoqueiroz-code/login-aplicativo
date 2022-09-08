12d89644701ee99f62dc793f6b6d831f2ecfd892
joaoqueiroz-code <joao.queiroz.code@gmail.com>
2022-09-07 22:52:59 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const login = () => {
+
+    let animation = `
+    <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_2rjsthcg.json"  
+    background="transparent"  speed="1"  style="width: 20em; height: 20em;"  loop  autoplay></lottie-player>
+    `;
+    
+    
+
+}
+
---
+++ js/main.js
@@ -0,0 +1,3 @@
+window.onload = () => {
+    //criar efeito de esmaecimento
+}
